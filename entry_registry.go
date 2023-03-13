28d257d4f210b9a2e47c3e2cf2cfbcafe23f2b48
Friedrich Große <friedrich.grosse@gmail.com>
2023-03-13 10:36:54 +0100

    Add unit tests for EntryRegistry

--- entry_registry.go
+++ entry_registry.go
@@ -12,13 +12,18 @@ type EntryRegistry struct {
 // EntryConstructor is the constructor function of a specific Entry implementation.
 type EntryConstructor func() Entry
 
-// NewEntryRegistry creates a new EntryRegistry. You can either pass the
-// constructor functions of all known Entry implementations or you can later
-// register them using EntryRegistry.Register(…).
+// NewEntryRegistry creates a new EntryRegistry. If you pass any constructor to
+// this function, each must create a unique Entry implementation (i.e. one which
+// returns a unique EntryType). Otherwise, this function panics.
+//
+// Alternatively, you can register the constructor functions using EntryRegistry.Register(…).
 func NewEntryRegistry(constructors ...EntryConstructor) *EntryRegistry {
 	r := &EntryRegistry{constructors: map[EntryType]EntryConstructor{}}
 	for _, newEntry := range constructors {
-		r.Register(newEntry)
+		err := r.Register(newEntry)
+		if err != nil {
+			panic(err)
+		}
 	}
 
 	return r
@@ -26,10 +31,19 @@ func NewEntryRegistry(constructors ...EntryConstructor) *EntryRegistry {
 
 // Register an EntryConstructor function. Each Entry will be registered with
 // the EntryType that is returned by the corresponding Entry.Type().
-func (r *EntryRegistry) Register(constructor EntryConstructor) {
+//
+// An error is returned if this constructor was already registered; i.e. a
+// constructor was already registered that creates an Entry with the same
+// EntryType as this constructor's Entry.
+func (r *EntryRegistry) Register(constructor EntryConstructor) error {
 	entry := constructor()
 	typ := entry.Type()
-	r.constructors[typ] = constructor // TODO: return error if this type is already registered
+	if existing, ok := r.constructors[typ]; ok {
+		return fmt.Errorf(`EntryType %x was already registered to type "%T"`, typ, existing())
+	}
+
+	r.constructors[typ] = constructor
+	return nil
 }
 
 // New instantiates a new Entry implementation that was previously registered
---
+++ entry_registry_test.go
@@ -0,0 +1,54 @@
+package wal
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestNewEntryRegistry(t *testing.T) {
+	r := NewEntryRegistry()
+	assert.NotNil(t, r)
+}
+
+func TestNewEntryRegistry_PanicIfRegisterTwice(t *testing.T) {
+	fun := func() {
+		NewEntryRegistry(
+			func() Entry { return new(ExampleEntry1) },
+			func() Entry { return new(ExampleEntry1) }, // same type registered twice
+		)
+	}
+
+	assert.PanicsWithError(t, `EntryType 0 was already registered to type "*wal.ExampleEntry1"`, fun)
+}
+
+func TestEntryRegistry_Register(t *testing.T) {
+	r := NewEntryRegistry()
+	err := r.Register(func() Entry { return new(ExampleEntry1) })
+	assert.NoError(t, err)
+
+	err = r.Register(func() Entry { return new(ExampleEntry2) })
+	assert.NoError(t, err)
+
+	err = r.Register(func() Entry { return new(ExampleEntry2) })
+	assert.EqualError(t, err, `EntryType 1 was already registered to type "*wal.ExampleEntry2"`)
+}
+
+func TestNewEntryRegistryNew(t *testing.T) {
+	r := NewEntryRegistry()
+	e, err := r.New(ExampleEntry1Type)
+	assert.EqualError(t, err, "unknown WAL entry type 0")
+	assert.Nil(t, e)
+
+	err = r.Register(func() Entry { return new(ExampleEntry1) })
+	require.NoError(t, err)
+
+	e, err = r.New(ExampleEntry1Type)
+	assert.NoError(t, err)
+	assert.IsType(t, new(ExampleEntry1), e)
+
+	e, err = r.New(ExampleEntry2Type)
+	assert.EqualError(t, err, "unknown WAL entry type 1")
+	assert.Nil(t, e)
+}
